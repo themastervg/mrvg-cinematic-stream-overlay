504edd2e4ca8a4f0f25a4e8d5161efe2127fbec8
themastervg <131874570+themastervg@users.noreply.github.com>
2026-02-15 19:35:57 +0530

    Docs Added

---
+++ docs/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+link.onclick=e=>{
+e.preventDefault();
+document.querySelector(link.getAttribute("href")).scrollIntoView({behavior:"smooth"})
+}
+})
